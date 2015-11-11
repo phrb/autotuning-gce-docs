d42fac246b0098e58aa4731b9d99fe94ce8bc35d
phrb <pedro.bruel@gmail.com>
2015-11-11 13:14:14 -0200

    Fix adddeps path.

--- experiments/adddeps.py
+++ experiments/adddeps.py
@@ -2,7 +2,7 @@
 
 import os
 
-path = "../opentuner/opentuner/utils/adddeps.py"
+path = "opentuner/opentuner/utils/adddeps.py"
 target = os.path.join(os.path.dirname(__file__), path)
 
 if os.path.isfile(target):
